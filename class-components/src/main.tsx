746a942171ae7aa4261de1be882a95477df51a27
Евгения Репникова <evrepni@gmail.com>
2025-01-31 12:47:47 +0500

    fix: fix eslint errors

---
+++ class-components/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+const rootContainer = document.getElementById('root');
+if (rootContainer !== null)
+  createRoot(rootContainer).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
